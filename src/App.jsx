b02add2da167ead10b52662fc05b3bf22bc9095e
Cristian da Rosa Melo <cristian_melo@sicredi.com.br>
2024-10-08 10:49:38 -0300

    feat: added redux to handle with favorite

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,63 @@
+// src/App.js
 import React, { useState, useEffect } from 'react';
 import ImageDisplay from './components/ImageDisplay';
 import ImageModal from './components/ImageModal';
 import SearchInput from './components/SearchInput';
+import { useSelector } from 'react-redux';
 
 const App = () => {
   const [images, setImages] = useState([]);
-  const [favorites, setFavorites] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [selectedImage, setSelectedImage] = useState(null);
+  const [showFavorites, setShowFavorites] = useState(false); // Estado para controlar a exibição dos favoritos
+
+  const favorites = useSelector((state) => state.favorites); // Seleciona os favoritos do Redux
 
   useEffect(() => {
     fetch('https://picsum.photos/v2/list')
       .then((res) => res.json())
       .then((data) => setImages(data))
+      .catch((err) => console.error('Failed to fetch images', err));
   }, []);
 
   const openModal = (image) => setSelectedImage(image);
   const closeModal = () => setSelectedImage(null);
 
-  const handleFavorite = (image) => {
-    setFavorites((prevFavorites) =>
-      prevFavorites.includes(image) ? prevFavorites : [...prevFavorites, image]
-    );
-  };
-
   const filteredImages = images.filter((image) =>
     image.author.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
+  const handleShowFavorites = () => {
+    setShowFavorites(!showFavorites);
+  };
+
   return (
     <main className="px-80">
-      <div className="pt-32 w-1/2">
+      <div className="pt-32 w-2/3">
         <span className="text-4xl font-bold">Faksplash</span>
         <h1 className="text-lg">
           Source of visual resources from the internet.
         </h1>
         <p className="text-lg pb-8">Provided by Picsum API.</p>
-        <SearchInput
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-          placeholder="Buscar por autor..."
-        />
+        <div className="flex items-center space-x-2">
+          <SearchInput
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            placeholder="Search..."
+          />
+          <button
+            onClick={handleShowFavorites}
+            className="flex items-center bg-gray-500 text-white px-4 py-2 rounded hover:bg-gray-600 transition"
+          >
+            {showFavorites ? '🔙 See all' : '💖 See fav'}
+          </button>
+        </div>
       </div>
 
       <div className="pt-32">
         <ImageDisplay
-          images={filteredImages}
-          onFavorite={handleFavorite}
+          images={showFavorites ? favorites : filteredImages}
           onClick={openModal}
-          favorites={favorites}
         />
       </div>
 
--- src/components/ImageDisplay/index.jsx
+++ src/components/ImageDisplay/index.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import { useDispatch, useSelector } from 'react-redux';
+import { addFavorite, removeFavorite } from '../../store/features';
 
-const ImageDisplay = ({ images, onFavorite, onClick, favorites }) => {
+const ImageDisplay = ({ images, onClick }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites);
+
+  const handleFavorite = (image) => {
+    if (favorites.find((fav) => fav.id === image.id)) {
+      dispatch(removeFavorite(image));
+    } else {
+      dispatch(addFavorite(image));
+    }
+  };
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
       {images.map((image) => {
-        const isFavorite = favorites.includes(image.id);
+        const isFavorite = favorites.includes(image);
         return (
           <div key={image.id} className="relative group mb-4">
             <img
@@ -19,11 +32,11 @@ const ImageDisplay = ({ images, onFavorite, onClick, favorites }) => {
               className="absolute top-2 right-2 bg-white bg-opacity-75 p-1 rounded-full transition-opacity duration-200 ease-in-out opacity-0 group-hover:opacity-100"
               onClick={(e) => {
                 e.stopPropagation();
-                onFavorite(image);
+                handleFavorite(image);
               }}
             >
               {isFavorite ? (
-                <AiFillHeart className="text-red-500 w-6 h-6" />
+                <AiFillHeart className="text-red-500 w-6 h-6 " />
               ) : (
                 <AiOutlineHeart className="text-gray-500 w-6 h-6 hover:text-gray-700 animate-pulse" />
               )}
