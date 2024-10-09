ba51f530f769c9c02f816d29351c3522962ba879
Cristian da Rosa Melo <cristian_melo@sicredi.com.br>
2024-10-09 13:01:22 -0300

    style: styled footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,12 +2,9 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="flex justify-between items-center pt-28 font-manrope bg-light-bg text-orange-500">
-      <span className="text-[36px]" aria-label="Nome do usuário">
-        cristian melo.
-      </span>
+    <footer className="fixed bottom-0 left-4 right-4 flex justify-end items-center pt-4 pb-4  bg-light-bg text-orange-500">
       <span className="text-custom-gray-80">
-        coding by <strong>me</strong>
+        coding by <strong>cristian melo</strong>
       </span>
     </footer>
   );
